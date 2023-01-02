aeb713a578ad90dba50dcea2738208e7fd019589
sind83 <sz.dymanski@gmail.com>
2023-01-02 21:31:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-// import { renderMovies, rerenderModalMovie } from './js/cards_rendering.js';
-
-// import { popularMovies } from './js/main_fetch';
-// import { pagination, paginationPlace } from './js/pagination.js';
-
 import './js/pagination';
