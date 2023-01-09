086f000f5bdf71b0112b8b496aa348692b62d370
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-01-09 21:41:25 +0100

    bugfix movie id and update movie search fetch

--- src/js/cards_rendering.js
+++ src/js/cards_rendering.js
@@ -51,7 +51,7 @@ export function renderMovies(page, results = [], genres) {
           releaseYear = release_date.slice(0, 4);
         }
         return `<div class='movie-card' data-movieId='${id}'>       
-          <img class='movie-card__image' src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}' loading='lazy' />
+          <img class='movie-card__image' src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}' data-movieId='${id}' loading='lazy' />
   <div class='movie-card__info'>
    <p class='info__title'>${homePageTitle}</p>
    <p class='info__adds'>${namesOfGenre} | ${releaseYear}</p>
