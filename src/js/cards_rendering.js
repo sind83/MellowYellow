adbdf051cbdaf91ba699de81dff038c406f2dfa5
Krzysztof Flisikowski <kflisikowski@10g.pl>
2023-01-10 11:02:44 +0100

    zmienony układ w cards rendering

--- src/js/cards_rendering.js
+++ src/js/cards_rendering.js
@@ -78,24 +78,60 @@ export function renderModalMovie({
     .map(genre => genre.name)
     .slice(0, 3)
     .join(', ');
-  const markup = `<div class='movie-card movie-card--modal' data-movieId='${id}'>
-          <img class='movie-card__image--modal' src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}' loading='lazy' />
-  <div class='movie-card__info movie-card__info--modal'>
-   <p class='info--modal__title'>${title}</p>
-   <ul class='info--modal__adds'>
-   <li class='adds__description'> Vote / Votes</li>
-   <li class='adds__description'> Popularity</li>
-   <li class='adds__description'> Original Title</li>
-   <li class='adds__description'> Genre</li></ul><ul> 
-   <li class='adds__value'> <span class='library'> ${vote_average}</span> / ${vote_count}</li>
-   <li class='adds__value'> ${popularity}</li>
-   <li class='adds__value adds__value--title'> ${original_title}</li>
-   <li class='adds__value'> ${genreNames}</li></ul>
-   <p class='adds__about'><span class='adds__value--title'>About</span> ${overview}</p>
-  <div class='adds__buttons'>
-   <button class='button button--inactive button--watched' type='button'>Add to watched</button>
-   <button class='button button--inactive button--queue' type='button'>Add to queue</button>     
-        </div></div></div>`;
+  const markup =
+    //   `<div class='movie-card movie-card--modal' data-movieId='${id}'>
+    //         <img class='movie-card__image--modal' src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}' loading='lazy' />
+    // <div class='movie-card__info movie-card__info--modal'>
+    //  <p class='info--modal__title'>${title}</p>
+    //  <ul class='info--modal__adds'>
+    //  <li class='adds__description'> Vote / Votes</li>
+    //  <li class='adds__description'> Popularity</li>
+    //  <li class='adds__description'> Original Title</li>
+    //  <li class='adds__description'> Genre</li></ul>
+    //  <ul>
+    //  <li class='adds__value'> <span class='library'> ${vote_average}</span> / ${vote_count}</li>
+    //  <li class='adds__value'> ${popularity}</li>
+    //  <li class='adds__value adds__value--title'> ${original_title}</li>
+    //  <li class='adds__value'> ${genreNames}</li></ul>
+
+    //  <p class='adds__about'><span class='adds__value--title'>About</span> ${overview}</p>
+    // <div class='adds__buttons'>
+    //  <button class='button button--inactive button--watched' type='button'>Add to watched</button>
+    //  <button class='button button--inactive button--queue' type='button'>Add to queue</button>
+    //       </div></div></div>`
+    `<div class="modal-container-film">
+                <img class="modal-img" src='https://image.tmdb.org/t/p/w500/${poster_path}' alt='${title}' loading='lazy'/>
+                <div class="film-details">
+                    <h2 class="film-details__main-title">${title}</h2>
+                    <ul class="details-list list">
+                        <li class="details-list__item">
+                            <p class="details-list_title">Vote / Votes</p>
+                            <span class="details-list__info1">${vote_average}</span>&nbsp/&nbsp<span
+                                class="details-list__info2">${vote_count}</span>
+                        </li>
+                        <li class="details-list__item">
+                            <p class="details-list_title">Popularity</p>
+                            <span class="details-list__info2">${popularity}</span>
+                        </li>
+                        <li class="details-list__item">
+                            <p class="details-list_title">Original Title</p>
+                            <span class="details-list__info2 ">${original_title}</span>
+                        </li>
+                        <li class="details-list__item">
+                            <p class="details-list_title">Genre</p>
+                            <span class="details-list__info2">${genreNames}</span>
+                        </li>
+                    </ul>
+                    <h3 class="film-details__secondary-title">About</h3>
+                    <p class="film-details__about">${overview}</p>
+                    <ul class="buttons-list list">
+                    </ul>
+                    <div class='adds__buttons'>
+    <button class='button button--inactive button--watched' type='button'>Add to watched</button>
+    <button class='button button--inactive button--queue' type='button'>Add to queue</button>
+    </div>
+                </div>
+            </div>`;
 
   const modalContent = document.querySelector('.modal-content');
   modalContent.insertAdjacentHTML('afterbegin', markup);
