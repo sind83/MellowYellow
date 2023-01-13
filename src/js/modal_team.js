455db83e157fa25602c1d563a0a4f7710b996e0f
Krzysztof Flisikowski <kflisikowski@10g.pl>
2023-01-13 10:46:48 +0100

    team images & search
    zmiany zgodnie z uwagami Mateusza. działają zdjęcia teamu i poprawione style  w searchu

--- src/js/modal_team.js
+++ src/js/modal_team.js
@@ -1,3 +1,10 @@
+import krzysztof from '../images/krzysztof.jpg';
+import malgorzata from '../images/malgorzata.jpg';
+import szymon from '../images/szymon.jpg';
+import dorota from '../images/dorota.jpg';
+import marta from '../images/marta.jpg';
+import mateusz from '../images/mateusz.jpg';
+
 export function modalTeam() {
   const teamMarkup = `
   <div class="team-details">
@@ -5,7 +12,7 @@ export function modalTeam() {
     <ul class="team-details__list list">
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/szymon.jpg" loading="lazy" />
+          <img class="team_img" src="${szymon}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Szymon Dymański</h3>
             <p class="team-details_role">Team Lider / Developer</p>
@@ -14,7 +21,7 @@ export function modalTeam() {
       </li>
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/dorota.jpg" loading="lazy" />
+          <img class="team_img" src="${dorota}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Dorota Domańska</h3>
             <p class="team-details_role">Scrum Master / Developer</p>
@@ -23,7 +30,7 @@ export function modalTeam() {
       </li>
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/malgorzata.jpg" loading="lazy" />
+          <img class="team_img" src="${malgorzata}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Małgorzata Marczyńska</h3>
             <p class="team-details_role">Grey Goose / Developer</p>
@@ -32,7 +39,7 @@ export function modalTeam() {
       </li>
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/marta.jpg" loading="lazy" />
+          <img class="team_img" src="${marta}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Marta Domżalska</h3>
             <p class="team-details_role">Developer</p>
@@ -41,7 +48,7 @@ export function modalTeam() {
       </li>
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/mateusz.jpg" loading="lazy" />
+          <img class="team_img" src="${mateusz}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Mateusz Martin</h3>
             <p class="team-details_role">Developer</p>
@@ -50,7 +57,7 @@ export function modalTeam() {
       </li>
       <li class="team-details__item">
         <figure>
-          <img class="team_img" src="./images/krzysztof.jpg" loading="lazy" />
+          <img class="team_img" src="${krzysztof}" loading="lazy" />
           <figcaption>
             <h3 class="team-details__name">Krzysztof Flisikowski</h3>
             <p class="team-details_role">Developer</p>
